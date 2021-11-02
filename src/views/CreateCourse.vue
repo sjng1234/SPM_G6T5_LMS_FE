9634ac0fad9df6405d5cbaece0137626f9dfdb50
Ng Shen Jie <sjng1234@ngs-macbook-pro.lan>
2021-11-02 10:45:03 +0800

    Sprint 2/Updated Create Course Vue Component

--- src/views/CreateCourse.vue
+++ src/views/CreateCourse.vue
@@ -1,26 +1,28 @@
 <template>
-    <div v-if="uploaded == true" class="alert alert-success" role="alert">
-    ✔ Successfully Uploaded!
+    <div>
+        <div v-if="uploaded == true" class="alert alert-success" role="alert">
+        ✔ Successfully Uploaded!
+        </div>
+        <div v-else-if="uploaded == false" class="alert alert-danger" role="alert">
+        ✖ Failed to upload - Please try again!
+        </div>
+        <h1>Create Course</h1>
+        <form class="container-fluid p-5" onsubmit="return false;">
+            <div class="input-group mb-3 mt-2">
+                <span class="input-group-text" id="basic-addon1">Course ID</span>
+                <input type="text" class="form-control" maxlength="6" style="text-transform:uppercase" placeholder="CS101" v-model="course_id" required>
+            </div>
+            <div class="input-group mb-3 mt-2">
+                <span class="input-group-text" id="basic-addon1">Course Name</span>
+                <input type="text" class="form-control" placeholder="Cybersecurity 101" v-model="course_name" required>
+            </div>
+            <div class="input-group mb-3 mt-2">
+                <span class="input-group-text" id="basic-addon1">Course Description</span>
+                <textarea type="text" class="form-control" placeholder="Cybersecurity 101 is an introductory course to cybersecurity where you get to learn..." v-model="course_description" required/>
+            </div>
+            <button type="submit" onsubmit="return false;" v-on:click="createCourse()" class="btn btn-primary">Submit</button>
+        </form>
     </div>
-    <div v-else-if="uploaded == false" class="alert alert-danger" role="alert">
-    ✖ Failed to upload - Please try again!
-    </div>
-    <h1>Create Course</h1>
-    <form class="container-fluid p-5" onsubmit="return false;">
-        <div class="input-group mb-3 mt-2">
-            <span class="input-group-text" id="basic-addon1">Course ID</span>
-            <input type="text" class="form-control" maxlength="6" style="text-transform:uppercase" placeholder="CS101" v-model="course_id" required>
-        </div>
-        <div class="input-group mb-3 mt-2">
-            <span class="input-group-text" id="basic-addon1">Course Name</span>
-            <input type="text" class="form-control" placeholder="Cybersecurity 101" v-model="course_name" required>
-        </div>
-        <div class="input-group mb-3 mt-2">
-            <span class="input-group-text" id="basic-addon1">Course Description</span>
-            <textarea type="text" class="form-control" placeholder="Cybersecurity 101 is an introductory course to cybersecurity where you get to learn..." v-model="course_description" required/>
-        </div>
-        <button type="submit" onsubmit="return false;" v-on:click="createCourse()" class="btn btn-primary">Submit</button>
-    </form>
 
 </template>
 
@@ -32,15 +34,12 @@ export default {
             course_id: "",
             course_name: "",
             course_description: "",
-            date_created: "",
-            course_creator_id: "HR111",
+            course_creator_id: 1,
             uploaded: null
         }
     },
     methods:{
         createCourse(){
-            this.date_created = new Date()
-
             var course_data = {
                 course_id: this.course_id,
                 course_name: this.course_name,
