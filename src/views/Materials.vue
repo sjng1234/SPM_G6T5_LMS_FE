955312fdac62e79ed2d3684f740b70e36da37563
Ng Shen Jie <ng.shen.jie1999@gmail.com>
2021-11-08 11:04:43 +0800

    Flagship Sprint/ Updated Materials.vue to reflect course progress, learners, can now see their course completion progress, quiz can only be done when all materials of the course are completed

--- src/views/Materials.vue
+++ src/views/Materials.vue
@@ -2,63 +2,64 @@
   <div class="container d-flex flex-column">
     <h1 class="text-start mb-2 mt-2">{{ course_id }}-{{class_id}} Materials</h1>
 
-    <div>
-      <button class="btn btn-primary d-flex mb-2 mt-2" @click="goToQuiz()">Take Quiz</button>
-    </div> 
-  
-  </div>
+    <div class="row">
+      <button v-if="checkAllCompleted()" class="btn btn-primary mb-2 mt-2" @click="goToQuiz()">Take Quiz</button>
+      <span class="bg-info mb-2 mt-2 p-2" v-else>Please complete all course materials to take quiz</span>
+    </div>
 
-  
+    <div class="container row">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Chapter</th>
+            <th scope="col">Title</th>
+            <th scope="col">Materials</th>
+          </tr>
+        </thead>
 
-  <div class="container row">
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">Chapter</th>
-          <th scope="col">Title</th>
-          <th scope="col">Materials</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr v-for="chapter in materials_data" v-bind:key="chapter.chapter_id">
-          <th scope="row">{{ chapter.chapter_id }}</th>
-          <td>{{ chapter.chapter_name }}</td>
-          <td>
-            <button
-              @click="toggleAccordion(chapter.chapter_id)"
-              class="btn btn-outline-info"
-              :aria-expanded="this.openState[chapter.chapter_id]"
-              :aria-controls="chapter.materials.material_id"
-            >
-              Expand Materials
-            </button>
-
-            <div v-if="this.openState[chapter.chapter_id]" v-bind:id="chapter.materials.material_id">
-              <div
-                v-for="material in chapter.materials"
-                v-bind:key="material.material_id"
-                class="col"
+        <tbody>
+          <tr v-for="chapter in materials_data" v-bind:key="chapter.chapter_id">
+            <th scope="row">{{ chapter.chapter_id }}</th>
+            <td>{{ chapter.chapter_name }}</td>
+            <td>
+              <button
+                @click="toggleAccordion(chapter.chapter_id)"
+                class="btn btn-outline-info"
+                :aria-expanded="this.openState[chapter.chapter_id]"
+                :aria-controls="chapter.materials.material_id"
               >
-                <br />
-                <div class="row">
-                  <div class="col-6">{{ material.material_name }}</div>
-                  <div class="col-6">
-                    <button
-                      type="button"
-                      class="btn btn-outline-secondary"
-                      @click="onDownload()"
-                    >
-                      Download
-                    </button>
+                Expand Materials
+              </button>
+
+              <div v-if="this.openState[chapter.chapter_id]" v-bind:id="chapter.materials.material_id">
+                <div
+                  v-for="material in chapter.materials"
+                  v-bind:key="material.material_id"
+                  class="col"
+                >
+                  <br />
+                  <div class="row align-items-center">
+                    <div class="col-6">{{ material.material_name }}</div>
+                    <div class="col-4">
+                      <button
+                        type="button"
+                        class="btn btn-outline-secondary"
+                        @click="{onDownload(); completeMaterial(material,chapter.chapter_id)}"
+                      >
+                        Download
+                      </button>
+                    </div>
+                    <div class="col-2">
+                      <span v-if="getCompleted(chapter.chapter_id,material.material_id)" class="badge bg-success">&#10003;</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -76,11 +77,12 @@ export default {
       course_id: "",
       class_id: "",
       openState: {},
+      materialCompletedState: {},
       materials_data: [],
+      course_material_count: 0,
     };
   },
   mounted() {
-    
     var [course_id, class_id] = this.id.split("-");
     this.course_id = course_id;
     this.class_id = class_id;
@@ -93,12 +95,14 @@ export default {
         this.materials_data = response.data;
         this.materials_data.forEach((chapter) => {
           this.openState[chapter.chapter_id] = false;
+          this.course_material_count += chapter.materials.length;
         });
       })
       .catch((error) => {
         console.log(error);
         this.materials_data = "error";
       });
+      this.checkCompletedMaterial();
   },
   methods: {
     onDownload() {
@@ -126,7 +130,45 @@ export default {
       var id = this.course_id + "-" + this.class_id;
       this.$router.push({ name: "Quiz", params: { id: id } });
     },
+    checkAllCompleted(){
+      return Object.keys(this.materialCompletedState).length === this.course_material_count;
+    },
+    checkCompletedMaterial() {
+      // Check material completion status
+      let url = `http://127.0.0.1:5000/learner/getCompletedMaterials/${this.course_id}-${this.class_id}-${this.$store.state.user_id}`;
+      console.log(url);
+      axios.get(url).then((response) => {
+        // console.log(response.data);
+        response.data.forEach((completed) => {
+          this.materialCompletedState[`${completed.chapter_id}-${completed.material_id}`] = true;
+        });
+        console.log(this.materialCompletedState);
+      });
+    },
+
+    getCompleted(chapter_id, material_id) {
+      return this.materialCompletedState[`${chapter_id}-${material_id}`];
+    },
     
+    completeMaterial(obj,chap_id) {
+      // Update material completion status when user download material
+      let url = "http://127.0.0.1:5000/learner/completeMaterial"
+      axios.put(url, {
+        learner_id: this.$store.state.user_id,
+        course_id: this.course_id,
+        class_id: this.class_id,
+        chapter_id: chap_id,
+        material_id: obj.material_id,
+        material_name: obj.material_name,
+        is_completed: true,
+      })
+      .then(()=>{
+        this.checkCompletedMaterial();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    },
   },
 };
 </script>
